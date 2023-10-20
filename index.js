70083b240511989eab64cb738140d0882deb37ee
Jawad Rashid <jawadrashid2010@gmail.com>
2023-10-20 19:16:25 +0500

    Using babel and CDN

---
+++ index.js
@@ -0,0 +1,15 @@
+var navbar = (
+    <nav>
+        <h1>Jawad's Burger Joint</h1>
+    
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
+);
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)

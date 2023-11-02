2448c674db5288e995a19cebcfde2cdceae5e47b
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-02 17:26:02 +0500

    Fetching data from api

--- components/Meme.js
+++ components/Meme.js
@@ -1,20 +1,41 @@
 import React from "react"
-import memesData from "../memesData.js"
 
 export default function Meme() {
+    /**
+     * Challenge: 
+     * As soon as the Meme component loads the first time,
+     * make an API call to "https://api.imgflip.com/get_memes".
+     * 
+     * When the data comes in, save just the memes array part
+     * of that data to the `allMemes` state
+     * 
+     * Think about if there are any dependencies that, if they
+     * changed, you'd want to cause to re-run this function.
+     * 
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` blocks to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
+     */
     
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
     
-    console.log(meme)
+    
+    React.useEffect(() => {
+        console.log("Use Effect Called")
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(jsonObj => setAllMemes(jsonObj.data.memes))
+    }, [])
+    
+    
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -24,12 +45,10 @@ export default function Meme() {
     
     function handleChange(event) {
         const {name, value} = event.target
-        setMeme(prevMeme => {
-            return {
-                ...prevMeme,
-                [name]: value
-            }
-        })
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
     
     return (

beb17262b28872c4957f40434d065d0388223231
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-04 00:03:57 +0500

    Installed Firebase

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3zPn2WUzwNACY0yw1GiSDJEDTc-q0gME",
+  authDomain: "react-notes-6a525.firebaseapp.com",
+  projectId: "react-notes-6a525",
+  storageBucket: "react-notes-6a525.appspot.com",
+  messagingSenderId: "531960400250",
+  appId: "1:531960400250:web:394d4a3da7d5e1318d6ec3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFireStore(app)
+const notesCollection = collection(db, "notes")

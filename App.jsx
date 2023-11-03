5a21e2287477496f386a8c197ea281642d42e0a2
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-04 01:32:12 +0500

    Cleanup

--- App.jsx
+++ App.jsx
@@ -14,19 +14,6 @@ export default function App() {
   const currentNote =
     notes.find((note) => note.id === currentNoteId) || notes[0];
 
-  /**
-   * Challenge:
-   * 1. ✅ Add createdAt and updatedAt properties to the notes
-   *    When a note is first created, set the `createdAt` and `updatedAt`
-   *    properties to `Date.now()`. Whenever a note is modified, set the
-   *    `updatedAt` property to `Date.now()`.
-   *
-   * 2. ✅ Create a new `sortedNotes` array (doesn't need to be saved
-   *    in state) that orders the items in the array from
-   *    most-recently-updated to least-recently-updated.
-   *    This may require a quick Google search.
-   */
-
   const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
 
   React.useEffect(() => {

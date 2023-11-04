5e07f3fba87a4dfae638af5c212438d423af9cc1
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-04 20:12:39 +0500

    Generating 10 dice elements using the array allNewDice

--- App.jsx
+++ App.jsx
@@ -2,29 +2,23 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+  const [diceArr, setDiceArr] = React.useState(allNewDice());
+
   function allNewDice() {
-    let numbers = [...Array(10).keys()]
-    let max = 6
-    let min = 1
-    var newNumbers = numbers.map((num) => Math.floor(Math.random() * (max - min + 1) + min))
-    console.log(newNumbers);
+    let numbers = [...Array(10).keys()];
+    let max = 6;
+    let min = 1;
+    return numbers.map((num) =>
+      Math.floor(Math.random() * (max - min + 1) + min)
+    );
   }
 
-  allNewDice()
+  const diceElements = diceArr.map(dice => <Die value={dice} />) 
 
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {diceElements}
       </div>
     </main>
   );

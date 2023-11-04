b1120f1c30fd06b4f3848cf1b22a827fbaf2be83
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-04 20:24:55 +0500

    Small change

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     let numbers = [...Array(10).keys()];
     let max = 6;
     let min = 1;
-    return numbers.map((num) =>
+    return numbers.map(() =>
       Math.floor(Math.random() * (max - min + 1) + min)
     );
   }

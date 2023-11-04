03c51d74b1e19dce656438e1431558ea80f96514
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-04 20:08:16 +0500

    Generate an array of random numbers

--- App.jsx
+++ App.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+  function allowNewDice() {
+    let numbers = [...Array(10).keys()]
+    let max = 6
+    let min = 1
+    var newNumbers = numbers.map((num) => Math.floor(Math.random() * (max - min + 1) + min))
+    console.log(newNumbers);
+  }
+
+  allowNewDice()
+
   return (
     <main>
-      <div class="dice-container">
+      <div className="dice-container">
         <Die value={1} />
         <Die value={2} />
         <Die value={3} />
--- Die.jsx
+++ Die.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Die(props) {
   return (
-    <div class="die-face">
+    <div className="die-face">
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
